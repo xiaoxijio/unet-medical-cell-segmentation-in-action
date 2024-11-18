70e34b0df1957f4fd605de02d80fab48700883d6
童童大dai王 <97423206+xiaoxijio@users.noreply.github.com>
2024-11-18 09:47:21 +0800

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,76 @@
+import os
+
+import cv2
+import numpy as np
+import torch
+import torch.utils.data
+
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, img_ids, img_dir, mask_dir, img_ext, mask_ext, num_classes, transform=None):
+        """
+        Args:
+            img_ids (list): Image ids.
+            img_dir: Image file directory.
+            mask_dir: Mask file directory.
+            img_ext (str): Image file extension.
+            mask_ext (str): Mask file extension.
+            num_classes (int): Number of classes.
+            transform (Compose, optional): Compose transforms of albumentations. Defaults to None.
+        
+        Note:
+            Make sure to put the files as the following structure:
+            <dataset name>
+            ├── images
+            |   ├── 0a7e06.jpg
+            │   ├── 0aab0a.jpg
+            │   ├── 0b1761.jpg
+            │   ├── ...
+            |
+            └── masks
+                ├── 0
+                |   ├── 0a7e06.png
+                |   ├── 0aab0a.png
+                |   ├── 0b1761.png
+                |   ├── ...
+                |
+                ├── 1
+                |   ├── 0a7e06.png
+                |   ├── 0aab0a.png
+                |   ├── 0b1761.png
+                |   ├── ...
+                ...
+        """
+        self.img_ids = img_ids
+        self.img_dir = img_dir
+        self.mask_dir = mask_dir
+        self.img_ext = img_ext
+        self.mask_ext = mask_ext
+        self.num_classes = num_classes
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.img_ids)
+
+    def __getitem__(self, idx):
+        img_id = self.img_ids[idx]
+
+        img = cv2.imread(os.path.join(self.img_dir, img_id + self.img_ext))
+
+        mask = []
+        for i in range(self.num_classes):
+            mask.append(cv2.imread(os.path.join(self.mask_dir, img_id + self.mask_ext),
+                                   cv2.IMREAD_GRAYSCALE)[..., None])
+        mask = np.dstack(mask)  # (H, W, num_classes)
+
+        if self.transform is not None:  # 参考https://github.com/albumentations-team/albumentations
+            augmented = self.transform(image=img, mask=mask)  # 同时对img和mask进行处理
+            img = augmented['image']  # 提取增强后的图像
+            mask = augmented['mask']  # 提取增强后的mask
+
+        img = img.astype('float32') / 255  # 归一化
+        img = img.transpose(2, 0, 1)
+        mask = mask.astype('float32') / 255
+        mask = mask.transpose(2, 0, 1)
+
+        return img, mask, {'img_id': img_id}
